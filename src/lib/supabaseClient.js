b8f36ef1509020170a09d1afd899efa248ab7389
Benito Canfora <47212597+dotthor@users.noreply.github.com>
2025-09-24 15:15:10 +0200

    Update Supabase client with placeholder API key
    Replaced Supabase API key with a placeholder.

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 /* import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://ppddtihaakyfivzrzzem.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwZGR0aWhhYWt5Zml2enJ6emVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk4OTMzNTgsImV4cCI6MjAxNTQ2OTM1OH0.RFG0bJ7xPLxd4vP6vQf7wNvDNovFE3_0fB_LmUQrtAI')
+export const supabase = createClient('https://ppddtihaakyfivzrzzem.supabase.co', 'xxx')
 
 // Create a function to handle inserts
 const handleInserts = (payload) => {
